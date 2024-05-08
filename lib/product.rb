3fcac5930397e3e9531c5e6d2a93e71cbfe7c6ff
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-08 18:03:35 +0100

    Add required keyword arguments to constructor to ensure products have a name, code, price (with corresponding tests)

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,9 @@
 class Product
   attr_reader :name, :product_code, :price
 
-  def initialize(name, product_code, price)
+  def initialize(name:, code:, price:)
     @name = name
-    @product_code = product_code
+    @product_code = code
     @price = price
   end
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,23 +1,44 @@
 require 'product.rb'
 
 RSpec.describe Product do
-  context 'when initialized with valid attributes' do
-    subject(:product) { Product.new('Strawberries', 'SR1', 5) }
+  describe '#initialize' do 
+    context 'with all keyword arguments' do
+      subject(:product) { 
+        Product.new(name: 'Strawberries', code: 'SR1', price: 5) 
+      }
+      it 'creates an instance of Product' do
+        expect(product).to be_kind_of(Product)
+      end
 
-    it 'is an instance of Product' do
-      expect(product).to be_kind_of(Product)
+      it 'sets the name attribute' do
+        expect(product.name).to eq('Strawberries')
+      end
+
+      it 'sets the code attribute' do
+        expect(product.product_code).to eq('SR1')
+      end
+
+      it 'sets the price attribute' do
+        expect(product.price).to eq(5)
+      end
     end
 
-    it 'sets the name attribute' do
-      expect(product.name).to eq('Strawberries')
+    context 'without any keyword arguments' do
+      it 'raises an ArgumentError' do
+        expect { Product.new('Strawberries', 'SR1', 5) }.to raise_error(ArgumentError)
+      end
     end
 
-    it 'sets the product_code attribute' do
-      expect(product.product_code).to eq('SR1')
+    context 'with one keyword argument missing' do
+      it 'raises an ArgumentError' do
+        expect { Product.new(name: 'Strawberries', code: 'SR1') }.to raise_error(ArgumentError)
+      end
     end
 
-    it 'sets the price attribute' do
-      expect(product.price).to eq(5)
+    context 'with no arguments at all' do
+      it 'raises an ArgumentError' do
+        expect { Product.new }.to raise_error(ArgumentError)
+      end
     end
   end
 end
