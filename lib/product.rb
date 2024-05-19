7dc28dfde64e98f8334ffee80364d58b87c7e897
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2024-05-19 17:32:04 +0100

    Add .attr_validated? class method to validate format for Product attributes and remove redundant tests
    - Remove instantiation tests to avoid redundancy
    - Add method to be called by classes that take input prior to instantiating a Product object
    - Calling class must pass key (symbol) alongside the attribute value to be checked/matched against regex, without trailing whitespaces and with letters upcased for a match to be found

--- lib/product.rb
+++ lib/product.rb
@@ -1,9 +1,20 @@
 class Product
-  attr_reader :name, :product_code, :price
+  attr_reader :name, :code, :price
+
+  INPUT_VAL_RULES = {
+    name: /\A[A-Z0-9]+(?:\s[A-Z0-9]+)*\z/, # At least 1 alphanumeric char, letters upcased, no symbols
+    code: /\A[A-Z]{1,4}\d{1,4}\z/, # 1-4 letters (upcased) followed by 1-4 digits, no symbols or whitespace
+    price: /^(?!0+$|0+\.0+$)\d{1,4}(\.\d{1,2})?$/ # 1-4 digits before decimal, up to 2 decimal places separated by periods (no commas allowed)
+  }
 
   def initialize(name:, code:, price:)
     @name = name
-    @product_code = code
+    @product = code
     @price = price
   end
+
+  def self.attr_validated?(key, value)
+    value.to_s.match(INPUT_VAL_RULES[key])
+  end
+
 end
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -1,45 +1,146 @@
-require 'product.rb'
+require_relative '../lib/product'
 
 RSpec.describe Product do
-  describe '#initialize' do 
-    context 'with all keyword arguments' do
-      subject(:product) { 
-        Product.new(name: 'Strawberries', code: 'SR1', price: 5) 
-      }
-      it 'creates an instance of Product' do
-        expect(product).to be_kind_of(Product)
+  describe '.attr_validated?' do
+    context 'when called with valid key-value pairs' do
+      context 'when called with valid name value' do
+        it 'validates single character' do
+          expect(Product.attr_validated?(:name, '7')).to be_truthy
+        end
+
+        it 'validates whitespace in between' do
+          expect(Product.attr_validated?(:name, 'GREEN TEA')).to be_truthy
+        end
+      end
+      
+      context 'when called with invalid name value' do
+        it 'invalidates lowercase letters' do
+          expect(Product.attr_validated?(:name, 'grEen teA')).to be_falsey
+        end
+        it 'invalidates trailing whitespaces' do
+          expect(Product.attr_validated?(:name, 'STRAWBERRIES ')).to be_falsey
+        end
+        it 'invalidates symbols' do
+          expect(Product.attr_validated?(:name, '*GREEN TEA*')).to be_falsey
+        end
       end
 
-      it 'sets the name attribute' do
-        expect(product.name).to eq('Strawberries')
+      context 'when called with valid code value' do
+        it 'validates a one letter one number' do
+          expect(Product.attr_validated?(:code, 'A1')).to be_truthy
+        end
+
+        it 'validates a combination of 4 letters followed by 4 numbers' do
+          expect(Product.attr_validated?(:code, 'ABCD1234')).to be_truthy
+        end
       end
 
-      it 'sets the code attribute' do
-        expect(product.product_code).to eq('SR1')
+      context 'when called with invalid code value' do
+        it 'invalidates lowercase letters' do
+          expect(Product.attr_validated?(:code, 'gr1')).to be_falsey
+        end
+        it 'invalidates trailing whitespaces' do
+          expect(Product.attr_validated?(:code, ' GR1 ')).to be_falsey
+        end
+        it 'invalidates only numbers' do
+          expect(Product.attr_validated?(:code, '1')).to be_falsey
+        end
+
+        it 'invalidates only letters' do
+          expect(Product.attr_validated?(:code, 'ABCD')).to be_falsey
+        end
+
+        it 'invalidates special symbols' do
+          expect(Product.attr_validated?(:code, 'SR#1')).to be_falsey
+        end
+
+        it 'invalidates whitespace in between' do
+          expect(Product.attr_validated?(:code, 'CF 1')).to be_falsey
+        end
+
+        it 'invalidates more than eight chars' do
+          expect(Product.attr_validated?(:code, 'ABCDE12345')).to be_falsey
+        end
       end
 
-      it 'sets the price attribute' do
-        expect(product.price).to eq(5)
+      context 'when called with valid price value' do
+        it 'validates price with one decimal place' do
+          expect(Product.attr_validated?(:price, '5.0')).to be_truthy
+        end
+
+        it 'validates price less than one' do
+          expect(Product.attr_validated?(:price, '0.99')).to be_truthy
+        end
+      end
+
+      context 'when called with invalid price value' do
+        it 'invalidates trailing whitespaces' do
+          expect(Product.attr_validated?(:price, ' 1 ')).to be_falsey
+        end
+        it 'invalidates zero' do
+          expect(Product.attr_validated?(:price, '0')).to be_falsey
+        end
+
+        it 'invalidates zero expressed with decimals' do
+          expect(Product.attr_validated?(:price, '0.00')).to be_falsey
+        end
+
+        it 'invalidates no digits before the dot' do
+          expect(Product.attr_validated?(:price, '.99')).to be_falsey
+        end
+
+        it 'invalidates above 9999' do
+          expect(Product.attr_validated?(:price, '10000')).to be_falsey
+        end
+
+        it 'invalidates more than two decimals' do
+          expect(Product.attr_validated?(:price, '5.000')).to be_falsey
+        end
+
+        it 'invalidates non-numeric characters' do
+          expect(Product.attr_validated?(:price, '5.0a')).to be_falsey
+        end
+
+        it 'invalidates non-numeric characters only' do
+          expect(Product.attr_validated?(:price, 'aaa')).to be_falsey
+        end
+
+        it 'invalidates commas' do
+          expect(Product.attr_validated?(:price, '5,00')).to be_falsey
+        end
+
+        it 'invalidates symbols' do
+          expect(Product.attr_validated?(:price, '$5.00')).to be_falsey
+        end
+
+        it 'invalidates whitespace in between' do
+          expect(Product.attr_validated?(:price, '5. 00')).to be_falsey
+        end
       end
     end
 
-    context 'without any keyword arguments' do
-      it 'raises an ArgumentError' do
-        expect { Product.new('Strawberries', 'SR1', 5) }.to raise_error(ArgumentError)
+    context 'when value is nil' do
+      it 'returns nil' do
+        expect(Product.attr_validated?(:name, nil)).to be_nil
       end
     end
 
-    context 'with one keyword argument missing' do
-      it 'raises an ArgumentError' do
-        expect { Product.new(name: 'Strawberries', code: 'SR1') }.to raise_error(ArgumentError)
+    context 'when key is nil' do
+      it 'fails' do
+        expect { Product.attr_validated?(nil, 'STRAWBERRIES') }.to raise_error(TypeError)
       end
     end
 
-    context 'with no arguments at all' do
-      it 'raises an ArgumentError' do
-        expect { Product.new }.to raise_error(ArgumentError)
+    context 'when key is not valid' do
+      it 'fails' do
+        expect { Product.attr_validated?(:unknown_key, 'STRAWBERRIES') }.to raise_error(TypeError)
+      end
+    end
+
+    context 'when key is not a symbol' do
+      it 'fails' do
+        expect { Product.attr_validated?('name', 'STRAWBERRIES') }.to raise_error(TypeError)
       end
     end
   end
 end
-
